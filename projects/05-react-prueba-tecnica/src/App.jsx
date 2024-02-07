eeb22c4ed97fa34c5577f6f95b15a72ee72d618e
lucasmoratonieto <lukmorato@gmail.com>
2024-02-07 19:20:52 +0100

    Final de prueba tecnica

--- projects/05-react-prueba-tecnica/src/App.jsx
+++ projects/05-react-prueba-tecnica/src/App.jsx
@@ -1,23 +1,43 @@
 import { useEffect, useState } from "react"
+import './index.css'
 
 const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
 // const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?fontSize=50&fontColor=red&json=true`
+const CAT_IMAGE_URL = 'https://cataas.com'
 
 export function App (){
 
     const [fact, setFact] = useState()
+    const[imageUrl, setImageUrl] = useState()
 
 
     useEffect(()=> {
         fetch(CAT_ENDPOINT_RANDOM_FACT)
             .then(res => res.json())
-            .then(data => setFact(data.fact))
+            .then(data =>{
+                const { fact } = data
+                setFact(fact)
+
+                const threeFirstWord = fact.split(' ', 3).join(' ')
+
+                fetch(`https://cataas.com/cat/says/${threeFirstWord}?size=50&color=red&json=true`)
+                    .then(res => res.json())
+                    .then(response => {
+                        const {mimetype} = response
+                        // console.log(mimetype)
+                        setImageUrl(mimetype)
+                    })
+            })
+            
     },[])
 
     return(
-        <main>
+        <main >
             <h1>App de gatitos</h1>
+            <div>
             {fact && <p>{fact}</p>}
+            {imageUrl && <img src={`${CAT_IMAGE_URL}${imageUrl}`} alt={imageUrl} />}
+            </div>
         </main>
     )
 }
